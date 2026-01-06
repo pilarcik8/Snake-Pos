cd1714b3c2dfff03f0c025c56c2814a84d123550
pilarcik8 <sompilarcikmichal@gmail.com>
2026-01-06 09:36:37 +0100

    Odstanene nutenie pisania ip adresy klientom

--- src/client/client_main.c
+++ src/client/client_main.c
@@ -3,20 +3,27 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
-        printf("Pouzitie: %s <address> <port>\n", argv[0]);
-        return 1;
-    }
+  char *address = "127.0.0.1";
 
-    const char *address = argv[1];
-    int port = atoi(argv[2]);
+  if (argc < 2) {
+    printf("Pouzitie: %s <port> [ip]\n", argv[0]);
+    return 1;
+  }
 
-    client_t client;
-    client_init(&client, address, port);
-    client_run(&client);
-    client_shutdown(&client);
+  int port = atoi(argv[1]);
+  if (port <= 0 || port > 65535) {
+    printf("Neplatny port\n");
+    return 1;
+  }
 
-    return 0;
+  if (argc >= 3) address = argv[2];
+
+  client_t client;
+  client_init(&client, address, port);
+  client_run(&client);
+  client_shutdown(&client);
+
+  return 0;
 }
 
 
